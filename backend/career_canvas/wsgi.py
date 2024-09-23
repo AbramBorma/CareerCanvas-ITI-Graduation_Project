302d89526e9b02816af6c873d024872332cae4bf
Mahmoud-A-Yehia <mahmoud.am.yehia@gmail.com>
2024-09-23 13:48:58 +0300

    setup

--- backend/career_canvas/wsgi.py
+++ backend/career_canvas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_canvas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_canvas.settings.production')
 
 application = get_wsgi_application()
