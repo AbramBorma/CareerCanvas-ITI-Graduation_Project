57134257f13d3417c1522a1eb190e6f2cdf47ef3
Abram Naguib <engabram.93@gmail.com>
2024-09-25 23:16:49 +0300

    Added gitignore file to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import PrivateRoute from "./utils/PrivateRoute"
-import { AuthProvider } from './context/AuthContext'
 
-import Registerpage from './components/Registerpage'
-import Loginpage from './components/Loginpage'
-import EditProfile from './components/EditProfile'
 
 
 import './App.css';
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home"
 import PortfolioForm from './components/PortfolioForm';
