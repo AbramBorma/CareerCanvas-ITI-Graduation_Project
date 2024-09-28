04e0f537123981fa5d89429f32f4704c6022b707
Ahmed Tarek <ahmedttaarek@gmail.com>
2024-09-29 00:42:25 +0300

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import Exam from './components/Exam';
 import CodeEditor from './components/CodeEditor';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
-import OrganizationDashboard from './components/organizationdashboard'; // Import the OrganizationDashboard component
+import OrganizationDashboard from './components/organizationdashboard'; 
 
 import { AuthProvider } from './context/AuthContext';
 
@@ -25,8 +25,6 @@ function App() {
         <Route path="/exams" element={<Exams />} />
         <Route path="/exams/:subject" element={<Exam />} />
         <Route path="/monaco/:subject" element={<CodeEditor />} />
-        
-        {/* Add the new route for OrganizationDashboard */}
         <Route path="/organization-dashboard" element={<OrganizationDashboard />} />
       </Routes>
     </div>
