68c519e32865871ec09173e98a05ca2a36964567
mariamabdk3m <mariamabdalmageid@gmail.com>
2024-10-01 17:06:29 +0300

    github

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,37 +1,17 @@
-from rest_framework.response import Response
+# backend/portfolio/views.py
 from rest_framework.views import APIView
-from django.core.cache import cache
-from career_canvas.tasks import run_github_scraping, run_hackerrank_scraping
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
-class GitHubDataView(APIView):
-    def get(self, request, username):
-        # Cache key for GitHub data
-        cache_key = f'github_data_{username}'
-        cached_data = cache.get(cache_key)
+User = get_user_model()
 
-        if cached_data:
-            # Return cached data if available
-            return Response(cached_data)
-        else:
-            # Trigger Celery task to scrape GitHub data
-            run_github_scraping.delay(username)
-            
-            # Inform the client that the scraping has started
-            return Response({'message': 'GitHub scraping started, try again later.'})
+class GitHubStatsView(APIView):
+    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
 
-
-class HackerRankDataView(APIView):
-    def get(self, request, username):
-        # Cache key for HackerRank data
-        cache_key = f'hackerrank_data_{username}'
-        cached_data = cache.get(cache_key)
-
-        if cached_data:
-            # Return cached data if available
-            return Response(cached_data)
-        else:
-            # Trigger Celery task to scrape HackerRank data
-            run_hackerrank_scraping.delay(username)
-
-            # Inform the client that the scraping has started
-            return Response({'message': 'HackerRank scraping started, try again later.'})
+    def get(self, request):
+        user = request.user  # Get the currently logged-in user
+        if user.github and 'github.com/' in user.github:
+            github_username = user.github.split('github.com/')[-1]
+            return Response({'github_username': github_username})
+        return Response({'error': 'GitHub link not found or invalid'}, status=400)
